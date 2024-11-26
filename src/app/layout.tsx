3aed2bcb3f0ff1087bb35c280ff43b6492577ba2
eric6923 <eric.2002@srit.org>
2024-11-26 15:16:43 +0530

    fav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,26 +22,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="icon"
-          type="image/png"
-          href="/favicon/favicon-96x96.png"
-          sizes="96x96"
-        />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="shortcut icon" href="favicon/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="favicon/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="favicon/site.webmanifest" />
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/site.webmanifest" />
+        
         <script
           defer
           data-site="YOUR_DOMAIN_HERE"
           src="https://api.nepcha.com/js/nepcha-analytics.js"
         ></script>
-        <link rel="shortcut icon" href="favicon/favicon.png" type="image/png" />
+        {/* <link rel="shortcut icon" href="favicon/favicon.png" type="image/png" /> */}
       </head>
       <body className={roboto.className}>
         <Layout>{children}</Layout>
